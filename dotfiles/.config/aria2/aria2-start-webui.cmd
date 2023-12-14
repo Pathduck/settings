61218755a3764c0661665f63df00286f9dd97c2a
Stian Lund <1493836+Pathduck@users.noreply.github.com>
2023-12-14 23:15:23 +0100

    del dotfiles

--- dotfiles/.config/aria2/aria2-start-webui.cmd
+++
@@ -1,8 +0,0 @@
-rem Starts Aria2 WebUI
-@echo off
-set Aria2=D:\bin\Aria2
-set Aria2WebUI=%Aria2%\webui-aria2
-
-start %Aria2%\aria2c --enable-rpc --rpc-listen-all --rpc-secret secret
-start %Aria2WebUI%\docs\index.html
-
