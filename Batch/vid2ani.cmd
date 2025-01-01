187e583b3b593aa05c0fc1ba76990a54ba5cd77f
Stian Lund <1493836+Pathduck@users.noreply.github.com>
2025-01-01 01:12:25 +0100

    updates

--- Batch/vid2ani.cmd
+++ Batch/vid2ani.cmd
@@ -1,29 +1,21 @@
 @ECHO OFF
-:: By: MDHEXT, Nabi KaramAliZadeh <nabikaz@gmail.com>
 :: Description: Video to GIF/APNG/WEBP converter
-:: Version: 5.5
-:: Url: https://github.com/MDHEXT/video2gif, forked from https://github.com/NabiKAZ/video2gif
+:: By: MDHEXT, Nabi KaramAliZadeh, Pathduck
+:: Version: 6.0
+:: Url: https://github.com/Pathduck/vid2ani/ forked from https://github.com/MDHEXT/video2gif
 :: What this script is based on: http://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html
 :: License: GNU General Public License v3.0 (GPLv3)
 
 :: Enable delayed variable expension
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-:: Storing Paths
-SET input="%~1"
-SET vid="%~dpnx1"
-SET output=%~dpn1
-SET FILEPATH=%~dp1
-
-:: Setting the path to the Working Directory
-SET WD=%TEMP%\VID2ANI
-
-:: Checking for blank input or help commands
-IF %input% == "" GOTO :help_message
-IF %input% == "-?" GOTO :help_message
-IF %input% == "/?" GOTO :help_message
-IF %input% == "help" GOTO :help_message
-IF %input% == "--help" GOTO :help_message
+:: Define ANSI Colors
+SET "OFF=[0m"
+SET "RED=[91m"
+SET "GREEN=[32m"
+SET "YELLOW=[33m"
+SET "BLUE=[94m"
+SET "CYAN=[96m"
 
 :: Clearing all variables
 SET "scale="
@@ -40,6 +32,20 @@ SET "version="
 SET "build="
 SET "loglevel="
 
+:: Check input
+SET input="%~1"
+SET output=%~dpn1
+
+:: Setting the path to the Working Directory
+SET WD=%TEMP%\VID2ANI
+
+:: Checking for blank input or help commands
+IF %input% == "" GOTO :help_message
+IF %input% == "-?" GOTO :help_message
+IF %input% == "/?" GOTO :help_message
+IF %input% == "help" GOTO :help_message
+IF %input% == "--help" GOTO :help_message
+
 GOTO :varin
 
 :varin
@@ -51,7 +57,7 @@ IF NOT "%~1" =="" (
 	IF "%~1" =="-d" SET "dither=%~2" & SHIFT
 	IF "%~1" =="-b" SET "bayerscale=%~2" & SHIFT
 	IF "%~1" =="-t" SET "filetype=%~2" & SHIFT
-	IF "%~1" =="-o" SET "output=%~dpnx2" & SHIFT
+	IF "%~1" =="-o" SET "output=%~dpn2" & SHIFT
 	IF "%~1" =="-s" SET "start_time=%~2" & SHIFT
 	IF "%~1" =="-e" SET "end_time=%~2" & SHIFT
 	IF "%~1" =="-c" SET "colormax=%~2" & SHIFT
@@ -66,9 +72,9 @@ GOTO :help_check_2
 :help_check_2
 :: Noob proofing the script to prevent it from breaking should critical settings not be defined
 IF NOT DEFINED scale SET "scale=-1"
-IF NOT DEFINED fps SET fps=15
-IF NOT DEFINED mode SET mode=1
-IF NOT DEFINED dither SET dither=0
+IF NOT DEFINED fps SET "fps=15"
+IF NOT DEFINED mode SET "mode=1"
+IF NOT DEFINED dither SET "dither=0"
 IF NOT DEFINED filetype SET "filetype=gif"
 IF NOT DEFINED loglevel SET "loglevel=error"
 
@@ -80,77 +86,98 @@ GOTO :safchek
 :: Output file type
 echo %filetype% | findstr /r "\<gif\> \<png\> \<apng\> \<webp\>" >nul
 IF %errorlevel% NEQ 0 (
-	ECHO [91mNot a valid file type[0m
+	ECHO %RED%Not a valid file type: %filetype%%OFF%
 	GOTO :EOF
 )
-IF "%filetype%"=="png" SET filetype=apng
-IF "%filetype%"=="apng" SET output=%output%.png
-IF "%filetype%"=="webp" SET output=%output%.webp
-IF "%filetype%"=="gif" SET output=%output%.gif
+IF "%filetype%"=="gif" SET "output=%output%.gif"
+IF "%filetype%"=="png" SET "filetype=apng"
+IF "%filetype%"=="apng" SET "output=%output%.png"
+IF "%filetype%"=="webp" SET "output=%output%.webp"
 
-:: Palettegen
+:: Validate Palettegen
 IF %mode% GTR 3 (
-	ECHO [91mNot a valid palettegen mode[0m
+	ECHO %RED%Not a valid palettegen ^(-m^) mode%OFF%
 	GOTO :EOF
 ) ELSE IF %mode% LSS 1 (
-	ECHO [91mNot a valid palettegen mode[0m
-	GOTO :EOF
-)
-:: Dithering
-IF %dither% GTR 8 (
-	ECHO [91mNot a valid dither algorithm[0m
-	GOTO :EOF
-) ELSE IF %dither% LSS 0 (
-	ECHO [91mNot a valid dither algorithm[0m
+	ECHO %RED%Not a valid palettegen ^(-m^) mode%OFF%
 	GOTO :EOF
 )
 
-::  Bayerscale
+:: Validate Dithering
+IF %dither% GTR 8 (
+	ECHO %RED%Not a valid dither ^(-d^) algorithm %OFF%
+	GOTO :EOF
+) ELSE IF %dither% LSS 0 (
+	ECHO %RED%Not a valid dither ^(-d^) algorithm%OFF%
+	GOTO :EOF
+)
+
+:: Validate Bayerscale
 IF DEFINED bayerscale (
 	IF !bayerscale! GTR 5 (
-		ECHO [91mNot a valid bayerscale value[0m
+		ECHO %RED%Not a valid bayerscale ^(-b^) value %OFF%
 		GOTO :EOF
 	) ELSE IF !bayerscale! LSS 0 (
-		ECHO [91mNot a valid bayerscale value[0m
+		ECHO %RED%Not a valid bayerscale ^(-b^) value%OFF%
 		GOTO :EOF
 	)
-	IF !bayerscale! LEQ 5 (
-		IF %dither% NEQ 1 (
-			ECHO [91mThis setting only works with bayer dithering[0m
+	IF %dither% NEQ 1 (
+		IF !bayerscale! LEQ 5 (
+			ECHO %RED%Bayerscale ^(-b^) only works with Bayer dithering%OFF%
 			GOTO :EOF
 		)
 	)
 )
 
-:: Lossy WEBP
+:: Validate Lossy WEBP
 IF DEFINED webp_lossy (
 	IF NOT "%filetype%" == "webp" (
-		ECHO [91mLossy is only valid for filetype webp[0m
+		ECHO %RED%Lossy ^(-l^) is only valid for filetype webp%OFF%
 		GOTO :EOF
 	) ELSE IF !webp_lossy! GTR 100 (
-		ECHO [91mNot a valid lossy quality value[0m
+		ECHO %RED%Not a valid lossy ^(-l^) quality value%OFF%
 		GOTO :EOF
 	) ELSE IF !webp_lossy! LSS 0 (
-		ECHO [91mNot a valid lossy quality value[0m
+		ECHO %RED%Not a valid lossy ^(-l^) quality value%OFF%
 		GOTO :EOF
 	)
 )
 
-:: Noob Proofing clipping
+:: Validate Clipping
 IF DEFINED start_time (
 	IF DEFINED end_time SET "trim=-ss !start_time! -to !end_time!"
 	IF NOT DEFINED end_time (
-		ECHO [91mPlease input the end time[0m
+		ECHO %RED%Please input the end time ^(-e^)%OFF%
+		GOTO :EOF
+	)
+)
+IF NOT DEFINED start_time (
+	IF DEFINED end_time (
+		ECHO %RED%Please input the start time ^(-s^)%OFF%
 		GOTO :EOF
 	)
 )
 
-IF NOT DEFINED start_time (
-	IF DEFINED end_time (
-		ECHO [91mPlease input the start time[0m
+:: Validate Framerate
+IF DEFINED fps (
+	IF !fps! LSS 0 (
+		ECHO  %RED%Framerate ^(-f^) must be greater than 0.%OFF%
 		GOTO :EOF
 	)
 )
+
+:: Validate Max Colors
+IF DEFINED colormax (
+	IF !colormax! LSS 3 (
+		ECHO  %RED%Max colors ^(-c^) must be between 3 and 256.%OFF%
+		GOTO :EOF
+	)
+	IF !colormax! GTR 256 (
+		ECHO  %RED%Max colors ^(-c^) must be between 3 and 256.%OFF%
+		GOTO :EOF
+	)
+)
+
 GOTO :script_start
 
 :script_start
@@ -164,22 +191,29 @@ FOR /F "delims=" %%a in ('ffmpeg -version') DO (
 )
 
 :: Displaying FFmpeg version string and creating the working directory
-ECHO [33m%version%[0m
-ECHO [33m%build%[0m
-ECHO [92mOutput file: %output%[0m
-ECHO [32mCreating working directory...[0m
+ECHO %YELLOW%%version%%OFF%
+ECHO %YELLOW%%build%%OFF%
+ECHO %GREEN%Output file:%OFF% %output%
+ECHO %GREEN%Creating working directory...%OFF%
 MD "%WD%"
 
 :palettegen
 :: Putting together command to generate palette
-SET palette=%WD%\palette
-SET frames=%palette%_%%05d
+SET palette=%WD%\palette_%%05d.png
 SET filters=fps=%fps%,scale=%scale%:-1:flags=lanczos
 
-:: Palettegen mode
-IF %mode% EQU 1 SET encode=palettegen=stats_mode=diff
-IF %mode% EQU 2 SET encode="palettegen=stats_mode=single"
-IF %mode% EQU 3 SET encode=palettegen
+:: APNG muxer does not support multiple palettes so fallback to using palettegen diff mode
+IF "%filetype%"=="apng" (
+	IF %mode% EQU 2 (
+		ECHO %YELLOW%APNG does not support multiple palettes - falling back to Palettegen mode 1 ^(diff^)%OFF%
+		SET mode=1
+	)
+)
+
+:: Palettegen encode mode
+IF %mode% EQU 1 SET "encode=palettegen=stats_mode=diff"
+IF %mode% EQU 2 SET "encode=palettegen=stats_mode=single"
+IF %mode% EQU 3 SET "encode=palettegen"
 
 :: Max colors
 IF DEFINED colormax (
@@ -188,22 +222,22 @@ IF DEFINED colormax (
 )
 
 :: Executing command to generate palette
-ECHO [32mGenerating palette...[0m
-ffmpeg -v %loglevel% %trim% -i %vid% -vf "%filters%,%encode%%mcol%" -y "%frames%.png"
+ECHO %GREEN%Generating palette...%OFF%
+echo ffmpeg -v %loglevel% %trim% -i %input% -vf "%filters%,%encode%%mcol%" -y "%palette%"
+ffmpeg -v %loglevel% %trim% -i %input% -vf "%filters%,%encode%%mcol%" -y "%palette%"
 
 :: Checking if the palette file is in the Working Directory, if not cleaning up
-IF NOT EXIST "%palette%_00001.png" (
-	IF NOT EXIST "%palette%.png" (
-		ECHO [91mFailed to generate palette file[0m
-		GOTO :cleanup
-	)
+IF NOT EXIST "%WD%\palette_00001.png" (
+	ECHO %RED%Failed to generate palette file%OFF%
+	GOTO :cleanup
 )
 
 :: Setting variables to put the encode command together
-:: Checking for Error Diffusion if using Bayer Scale and adjusting the command accordingly
-IF %mode% EQU 1 SET decode=paletteuse
+
+:: Palettegen decode mode
+IF %mode% EQU 1 SET "decode=paletteuse"
 IF %mode% EQU 2 SET "decode=paletteuse=new=1"
-IF %mode% EQU 3 SET decode=paletteuse
+IF %mode% EQU 3 SET "decode=paletteuse"
 
 :: Error diffusion
 IF DEFINED errorswitch (
@@ -212,40 +246,42 @@ IF DEFINED errorswitch (
 	IF %mode% EQU 3 SET "errordiff==diff_mode=rectangle"
 )
 
-:: WEBP pixel format and lossy quality
-IF "%filetype%" == "webp" (
-	IF DEFINED webp_lossy (
-		SET "webp_lossy=-lossless 0 -pix_fmt yuv420p -quality %webp_lossy%"
-	) ELSE SET "webp_lossy=-lossless 1"
-)
+:: Prepare dithering and encoding options
+IF %dither% EQU 0 SET "ditheralg=none"
+IF %dither% EQU 1 SET "ditheralg=bayer"
+IF %dither% EQU 2 SET "ditheralg=heckbert"
+IF %dither% EQU 3 SET "ditheralg=floyd_steinberg"
+IF %dither% EQU 4 SET "ditheralg=sierra2"
+IF %dither% EQU 5 SET "ditheralg=sierra2_4a"
+IF %dither% EQU 6 SET "ditheralg=sierra3"
+IF %dither% EQU 7 SET "ditheralg=burkes"
+IF %dither% EQU 8 SET "ditheralg=atkinson"
 
-:: Dither algorithm
-IF %dither% EQU 0 SET ditheralg=none
-IF %dither% EQU 1 SET ditheralg=bayer
-IF %dither% EQU 2 SET ditheralg=heckbert
-IF %dither% EQU 3 SET ditheralg=floyd_steinberg
-IF %dither% EQU 4 SET ditheralg=sierra2
-IF %dither% EQU 5 SET ditheralg=sierra2_4a
-IF %dither% EQU 6 SET ditheralg=sierra3
-IF %dither% EQU 7 SET ditheralg=burkes
-IF %dither% EQU 8 SET ditheralg=atkinson
-
+:: Paletteuse error diffusion
 IF NOT %mode% EQU 2 (
-	IF DEFINED errorswitch SET ditherenc=:dither=!ditheralg!
-	IF NOT DEFINED errorswitch SET ditherenc==dither=!ditheralg!
-) ELSE SET ditherenc=:dither=!ditheralg!
+	IF DEFINED errorswitch SET "ditherenc=:dither=!ditheralg!"
+	IF NOT DEFINED errorswitch SET "ditherenc==dither=!ditheralg!"
+) ELSE SET "ditherenc=:dither=!ditheralg!"
 
 :: Checking for Bayer Scale and adjusting command
 IF NOT DEFINED bayerscale SET "bayer="
-IF DEFINED bayerscale SET bayer=:bayer_scale=%bayerscale%
+IF DEFINED bayerscale SET "bayer=:bayer_scale=%bayerscale%"
+
+:: WEBP pixel format and lossy quality
+IF "%filetype%" == "webp" (
+	IF DEFINED webp_lossy (
+		SET "webp_lossy=-lossless 0 -pix_fmt yuva420p -quality %webp_lossy%"
+	) ELSE SET "webp_lossy=-lossless 1"
+)
 
 :: Executing the encoding command
-ECHO [32mEncoding animation...[0m
-ffmpeg -v %loglevel% %trim% -i %vid% -thread_queue_size 512 -i "%frames%.png" -lavfi "%filters% [x]; [x][1:v] %decode%%errordiff%%ditherenc%%bayer%" -f %filetype% %webp_lossy% -loop 0 -plays 0 -y "%output%"
+ECHO %GREEN%Encoding animation...%OFF%
+echo ffmpeg -v %loglevel% %trim% -i %input% -thread_queue_size 512 -i "%palette%" -lavfi "%filters% [x]; [x][1:v] %decode%%errordiff%%ditherenc%%bayer%" -f %filetype% %webp_lossy% -loop 0 -plays 0 -y "%output%"
+ffmpeg -v %loglevel% %trim% -i %input% -thread_queue_size 512 -i "%palette%" -lavfi "%filters% [x]; [x][1:v] %decode%%errordiff%%ditherenc%%bayer%" -f %filetype% %webp_lossy% -loop 0 -plays 0 -y "%output%"
 
 :: Checking if file was created and cleaning up if not
 IF NOT EXIST "%output%" (
-	ECHO [91mFailed to generate animation[0m
+	ECHO %RED%Failed to generate animation%OFF%
 	GOTO :cleanup
 )
 
@@ -254,96 +290,60 @@ IF DEFINED picswitch START "" "%output%"
 
 :cleanup
 :: Cleaning up
-ECHO [32mDeleting temporary files...[0m
+ECHO %GREEN%Deleting temporary files...%OFF%
 RMDIR /S /Q "%WD%"
+ECHO %GREEN%Done.%OFF%
 ENDLOCAL
-ECHO [93mDone![0m
 GOTO :EOF
 
 :help_message
 :: Print usage message
 ECHO:
-ECHO [92mVideo to GIF/APNG/WEBP converter v5.5[0m
-ECHO [96m^(C^) 2017-2022, MDHEXT ^&^ Nabi KaramAliZadeh ^<nabikaz@gmail.com^>[0m
+ECHO %GREEN%Video to GIF/APNG/WEBP converter v6.0%OFF%
+ECHO %BLUE%By MDHEXT, Nabi KaramAliZadeh, Pathduck%OFF%
 ECHO:
-ECHO You can download this fork from here:[0m
-ECHO [96mhttps://github.com/MDHEXT/video2gif[0m
-ECHO You can download the original release here:
-ECHO [96mhttps://github.com/NabiKAZ/video2gif[0m
-ECHO This tool uses ffmpeg, you can download that here:
-ECHO [96mhttps://www.ffmpeg.org/download.html#build-windows[0m
-ECHO This tool wouldn't be possible without the research listed here:
-ECHO [96mhttps://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html[0m
+ECHO %GREEN%Usage:%OFF%
+ECHO %~nx0 [input_file] [arguments]
 ECHO:
-ECHO [92mUsage:[0m
-ECHO vid2ani [input_file] [Arguments]
+ECHO %GREEN%Arguments:%OFF%
+ECHO  -t  Output file type. Valid: 'gif' (default), 'apng', 'png', 'webp'.
+ECHO  -o  Output file. The default is the same name as the input video.
+ECHO  -r  Scale or size. Width of the animation in pixels.
+ECHO  -s  Start time of the animation (HH:MM:SS.MS).
+ECHO  -e  End time of the animation (HH:MM:SS.MS).
+ECHO  -f  Framerate in frames per second (default: 15).
+ECHO  -d  Dithering algorithm to be used (default: 0).
+ECHO  -b  Bayer Scale setting. Range 0 - 5 (default: 2).
+ECHO  -m  Palettegen mode: 1 (diff), 2 (single), 3 (full) (default: 1).
+ECHO  -c  Maximum colors usable per palette. Range 3 - 256 (default).
+ECHO  -k  Enables paletteuse error diffusion.
+ECHO  -l  Enable lossy WebP compression and quality. Range 0 - 100.
+ECHO  -v  Set FFmpeg log level (default: error).
+ECHO  -p  Opens the resulting animation in the default viewer.
 ECHO:
-ECHO [92mArguments:[0m
+ECHO %GREEN%Dithering Mode%OFF%
+ECHO  0: None
+ECHO  1: Bayer
+ECHO  2: Heckbert
+ECHO  3: Floyd Steinberg
+ECHO  4: Sierra2
+ECHO  5: Sierra2_4a
+ECHO  6: Sierra3
+ECHO  7: Burkes
+ECHO  8: Atkinson
 ECHO:
-ECHO	-t	Output file type.
-ECHO		[33mValid: 'gif' (default), 'png', 'webp'.[0m
+ECHO %GREEN%Palettegen Modes%OFF%
+ECHO  1: diff - only what moves affects the palette
+ECHO  2: single - one palette per frame
+ECHO  3: full - one palette for the whole animation
 ECHO:
-ECHO	-o	Output file.
-ECHO		[33mThe default is the same name as the input video.[0m
-ECHO:
-ECHO	-r	Scale or size.
-ECHO		[96mWidth of the animation in pixels.[0m
-ECHO		[33mThe default is the same scale as the original video.[0m
-ECHO:
-ECHO	-s	Start time of the animation [96m(HH:MM:SS.MS)[0m
-ECHO:
-ECHO	-e	End time of the animation [96m(HH:MM:SS.MS)[0m
-ECHO:
-ECHO	-f	Framerate in frames per second.
-ECHO		[33mThe default is 15.[0m
-ECHO:
-ECHO	-d	Dithering algorithm to be used.
-ECHO		[33mThe default is 0 (None).[0m
-ECHO:
-ECHO	-b	Bayer Scale setting.
-ECHO		[96mThis can only be used when Bayer dithering is applied.
-ECHO		[33mRange 0 - 5, default is 2.[0m
-ECHO:
-ECHO	-m	Palettegen mode - one of 3 modes listed below.
-ECHO		[33mThe default is 1 (diff).[0m
-ECHO:
-ECHO	-c	The maximum amount of colors useable per palette.
-ECHO		[33mRange 3 - 256 (default)[0m
-ECHO:
-ECHO	-k	Enables paletteuse error diffusion.
-ECHO:
-ECHO	-l	Enable lossy WebP compression and quality.
-ECHO		[96mThe default for WebP is lossless.[0m
-ECHO		[33mRange 0 - 100, default 75.[0m
-ECHO:
-ECHO	-v	Set FFmpeg log level, for troubleshooting.
-ECHO		[33mThe default log level is 'error'[0m
-ECHO:
-ECHO	-p	Opens the resulting animation in your default Photo Viewer.
-ECHO:
-ECHO [92mDithering Modes:[0m
-ECHO 0: None
-ECHO 1: Bayer
-ECHO 2: Heckbert
-ECHO 3: Floyd Steinberg
-ECHO 4: Sierra2
-ECHO 5: Sierra2_4a
-ECHO 6: sierra3
-ECHO 7: burkes
-ECHO 8: atkinson
-ECHO:
-ECHO [92mPalettegen Modes:[0m
-ECHO 1: diff - only what moves affects the palette
-ECHO 2: single - one palette per frame
-ECHO 3: full - one palette for the whole animation
-ECHO:
-ECHO [92mAbout Bayerscale:[0m
+ECHO %GREEN%About Bayerscale%OFF%
 ECHO When bayer dithering is selected, the Bayer Scale option defines the
 ECHO scale of the pattern (how much the crosshatch pattern is visible).
 ECHO A low value means more visible pattern for less banding, a higher value
 ECHO means less visible pattern at the cost of more banding.
 ECHO:
-ECHO [92mPeople who made this project come to fruition:[0m
+ECHO %GREEN%People who made this project come to fruition%OFF%
 ECHO ubitux, Nabi KaramAliZadeh, and the very kind and patient people in the
 ECHO Batch Discord Server. Without these people's contributions, this script
 ECHO would not be possible. Thank you all for your contributions and
