71e1610ffd8c4da075a27f2da51740cef9ab3844
Stian <stianlund@gmail.com>
2025-01-21 10:29:07 +0100

    update

--- Batch/genHTMLIndex.sh
+++ Batch/genHTMLIndex.sh
@@ -9,9 +9,9 @@ exclude_paths=(
 # Get array of dirs
 dirs=( $(find $@ -type d ${exclude_paths[@]}) )
 
-for i in "${!dirs[@]}"
-do 
-	# Check for existing index made by 'tree' (rc=0) or nonexistent (rc=2)
+for i in "${!dirs[@]}"; do 
+	# Check for existing index made by 'tree' (rc=0) 
+	# or nonexistent (rc=2)
 	grep -qsm1 "Made by 'tree'" "${dirs[i]}/index.html"
 	if  [ $? -eq 1 ]; then 
 		unset 'dirs[i]'
@@ -28,18 +28,21 @@ echo
 
 # Get confirm for creation of index
 read -n1 -p "Continue? (y/N) " confirm
-if ! echo $confirm | grep -q '^[Yy]$'; then echo -e "\nAborting"; exit 1; fi
+if ! echo $confirm | grep -q '^[Yy]$'; then 
+	echo -e "\nAborting"; exit 1
+fi
 echo
 
 # Create the indexes
-for i in "${!dirs[@]}"
-do 
+for i in "${!dirs[@]}"; do 
 	cd "${dirs[i]}"
 	echo "Creating index in ${dirs[i]}"
 	# Generate HTML index - Color, Date, Size(h), No lines(i), Title(T), filter self(I)
 	# Pipe to sed: Remove credits, link to parent dir, change colours
-	tree -CDhi -H "." -T "$(basename $PWD)" -I "index.html" -L 1 --dirsfirst --charset "utf-8"\
-	| sed -e '/<hr>/,+7d' -e 's/href="\.">\./href="..">../'\
+	tree -CDhi -H "." -T "$(basename $PWD)" -I "index.html" -L 1\
+	--dirsfirst --charset "utf-8"\
+	| sed\
+	-e '/<hr>/,+7d' -e 's/href="\.">\./href="..">../'\
 	-e 's/BODY {/BODY { color: lime; background: black;/'\
 	-e 's/color: black;/color: lime;/'\
 	-e 's/color: blue;/color: deepskyblue;/'\
