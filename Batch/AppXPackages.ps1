34337673d7de0fb7249f1f1abb23e3ba82034d41
Stian <stianlund@gmail.com>
2025-06-18 12:22:57 +0200

    Update files

--- Batch/AppXPackages.ps1
+++ Batch/AppXPackages.ps1
@@ -1,20 +1,19 @@
 # Get-AppxPackage, Get-AppxProvisionedPackage
 
-Write-Output "     AppxPackages"
-Write-Output "======================"
+Write-Output "AppxPackages"
+Write-Output "============"
 
 #Get-AppxPackage -AllUsers
 Get-AppxPackage -Allusers * | Select Name, PackageFullName
 
 #Get-AppxPackage *Windows.DevHome* | Remove-AppxPackage
 #Get-AppxPackage -AllUsers -PackageTypeFilter Bundle -Name "*Windows.DevHome*" | Remove-AppxPackage
-#Get-appxProvisionedPackage -online | where-object {$_.packagename -like '*Windows.DevHome*'} | remove-appxprovisionedpackage -online  
 
 Write-Output "AppxProvisionedPackages"
 Write-Output "======================="
 
 Get-AppxProvisionedPackage -online | select PackageName
 
-#Get-AppxProvisionedpackage –online | where-object {$_.packagename –like “*xbox*”} | Remove-AppxProvisionedPackage -online
-#Get-AppxProvisionedpackage –online | where-object {$_.packagename –notlike “*store*”} | Remove-AppxProvisionedPackage -online
+#Get-AppxProvisionedpackage –online | where-object {$_.packagename –like "*Windows.DevHome*"} | Remove-AppxProvisionedPackage -online
+#Get-AppxProvisionedpackage –online | where-object {$_.packagename –notlike "*Windows.DevHome*"} | Remove-AppxProvisionedPackage -online
 #Remove-AppxProvisionedPackage -online -packagename <string>
