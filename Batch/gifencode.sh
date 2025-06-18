32ed218fc36e34610dd5c13edad3a22d4ea640cc
Stian <1493836+Pathduck@users.noreply.github.com>
2025-06-18 12:22:57 +0200

    Update files

--- Batch/gifencode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-palette="palette.png"
-filters="fps=15,scale=0:-1:flags=lanczos"
-
-ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
-ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -plays 0 -y $2
-rm -f $palette
