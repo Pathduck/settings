42f9e156a69e0121a117e9081726628424ceb943
Stian <stianlund@gmail.com>
2026-01-16 11:04:30 +0100

    Update files

--- Batch/save-StoneShard.cmd
+++ Batch/save-StoneShard.cmd
@@ -1,9 +1,13 @@
 @echo off
-set game_dir="%localappdata%\StoneShard"
+set game_dir=%localappdata%\StoneShard
 set save_dir="f:\My Games\Stoneshard"
 
 for /f "tokens=1-3 delims=. " %%a in ("%DATE%") do (set mydate=%%c-%%b-%%a)
 for /f "tokens=1-3 delims=: " %%a in ("%TIME%") do (set mytime=%%a%%b%%c)
-	
-cd /d %game_dir%
-zip -qr %save_dir%\%mydate%-%mytime%.zip *
+
+if exist %game_dir% (
+	cd /d %game_dir%
+	zip -qr %save_dir%\%mydate%-%mytime%.zip *
+) else (
+	echo Game dir not found!
+)
