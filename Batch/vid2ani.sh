8eb9727996840dd139cf2f5c7dd85b0117e5ba79
Stian <stianlund@gmail.com>
2026-02-07 13:55:11 +0100

    Update files

--- Batch/vid2ani.sh
+++ Batch/vid2ani.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Description: Video to GIF/APNG/WEBP converter
 # By: MDHEXT, Nabi KaramAliZadeh, Pathduck
-# Version: 6.0
+# Version: 6.1
 # Url: https://github.com/Pathduck/vid2ani/
 # License: GNU General Public License v3.0 (GPLv3)
 
@@ -29,22 +29,11 @@ esac
 
 # Assign input and output
 input="$1"
-output="${input%.*}"
+output=$(basename "${input%.*}")
 
 # Validate input file
 if [[ ! -f "$input" ]]; then
 	echo ${RED}"Input file not found: $input"${OFF}; exit 1
-fi
-
-# Fix paths for Cygwin and create working directory
-if [[ "$(uname)" == *"CYGWIN"* ]]; then
-	# Use Windows-compatible directories for Cygwin
-	input=$(cygpath -w "$input")
-	output=$(cygpath -w "$output")
-	WD=$(cygpath -w "$(mktemp -d -t vid2ani-XXXXXX)")
-else
-	# Use POSIX-compatible directories
-	WD=$(mktemp -d -t vid2ani-XXXXXX)
 fi
 
 # Cleanup on exit, interrupt, termination
@@ -57,14 +46,16 @@ dither=0
 scale="-1"
 filetype="gif"
 webp_lossy=""
-webp_lossy_def=75
+webp_lossy_q=75
 loglevel="error"
 bayerscale=""
 colormax=""
 start_time=""
 end_time=""
+crop=""
 errorswitch=""
 picswitch=""
+playswitch=""
 
 # Parse Arguments
 shift
@@ -73,8 +64,9 @@ while [[ $# -gt 0 ]]; do
 		-o) output="${2%.*}"; shift;;
 		-t) filetype="$2"; shift;;
 		-r) scale="$2"; shift;;
-		-l) if [[ "$2" =~ ^[0-9]+$ ]]; then webp_lossy="$2"; shift
-			else webp_lossy=$webp_lossy_def; fi ;;
+		-l) if [[ "$2" =~ ^[0-9]+$ ]]; then 
+			webp_lossy=1; webp_lossy_q="$2"; shift
+			else webp_lossy=1; fi ;;
 		-f) fps="$2"; shift;;
 		-s) start_time="$2"; shift;;
 		-e) end_time="$2"; shift;;
@@ -83,12 +75,19 @@ while [[ $# -gt 0 ]]; do
 		-m) mode="$2"; shift;;
 		-c) colormax="$2"; shift;;
 		-v) loglevel="$2"; shift;;
+		-x) crop="$2"; shift;;
 		-k) errorswitch=1;;
 		-p) picswitch=1;;
+		-y) playswitch=1;;
 		*) echo ${RED}"Unknown option $1"${OFF}; exit 1;;
 	esac
 	shift
 done
+
+# Validate if output is a directory and use input file name
+if [[ -d "$output" ]]; then
+	output="$output/"$(basename "${input%.*}")
+fi
 
 # Validate output file extension
 case "$filetype" in
@@ -124,14 +123,14 @@ if [[ -n "$webp_lossy" ]]; then
 	if [[ "$filetype" != "webp" ]]; then
 		echo ${RED}"Lossy (-l) is only valid for filetype webp"${OFF}; exit 1
 	fi
-	if [[ "$webp_lossy" -gt 100 || "$webp_lossy" -lt 0 ]]; then
+	if [[ "$webp_lossy_q" -gt 100 || "$webp_lossy_q" -lt 0 ]]; then
 		echo ${RED}"Not a valid lossy (-l) quality value"${OFF}; exit 1
 	fi
 fi
 
 # Validate Clipping
 if [[ -n "$start_time" && -z "$end_time" ]]; then
-	echo ${RED}"End time (-e) is required when Start time (-e) is specified."${OFF}; exit 1
+	echo ${RED}"End time (-e) is required when Start time (-s) is specified."${OFF}; exit 1
 elif [[ -n "$end_time" && -z "$start_time" ]]; then
 	echo ${RED}"Start time (-s) is required when End time (-e) is specified."${OFF}; exit 1
 elif [[ -n "$end_time" && -n "$start_time" ]]; then
@@ -148,16 +147,33 @@ if [[ -n "$colormax" && "$colormax" -lt 3 || "$colormax" -gt 256 ]]; then
 	echo ${RED}"Max colors (-c) must be between 3 and 256."${OFF}; exit 1
 fi
 
-# Displaying FFmpeg version string
-ffmpeg_version=$(ffmpeg -version | head -n2)
-echo ${YELLOW}"$ffmpeg_version"${OFF}
+# Fix paths for Cygwin and create working directory
+if [[ "$(uname)" == *"CYGWIN"* ]]; then
+	# Use Windows-compatible directories for Cygwin
+	input=$(cygpath -w "$input")
+	output=$(cygpath -w "$output")
+	WD=$(cygpath -w "$(mktemp -d -t vid2ani-XXXXXX)")
+else
+	# Use POSIX-compatible directories
+	WD=$(mktemp -d -t vid2ani-XXXXXX)
+fi
+
+# Putting together command to generate palette
+palette="$WD/palette_%05d.png"
+filters="crop=$crop,fps=$fps,scale=$scale:-1:flags=lanczos"
+
+# FFplay preview
+if [[ -n "$playswitch" ]]; then
+	echo ${YELLOW}"$(ffplay -version | head -n2)"${OFF}
+	ffplay -v "${loglevel}" -i "${input}" -vf "${filters}" -an -loop 0 -ss ${start_time:-0} -t ${end_time:-3} 
+	exit 0
+fi
+
+# Displaying FFmpeg version string and output file
+echo ${YELLOW}"$(ffmpeg -version | head -n2)"${OFF}
 echo ${GREEN}Output file:${OFF} $output
 
-## Putting together command to generate palette ##
-palette="$WD/palette_%05d.png"
-filters="fps=$fps,scale=$scale:-1:flags=lanczos"
-
-# APNG muxer does not support multiple palettes so fallback to using palettegen diff mode
+# APNG muxer does not support multiple palettes, fallback to palettegen diff mode
 if [[ "$filetype" == "apng" && "$mode" -eq 2 ]]; then
 	echo ${YELLOW}"APNG does not support multiple palettes - falling back to Palettegen mode 1 (diff)"${OFF}
 	mode=1
@@ -238,14 +254,16 @@ if [[ -z "$bayerscale" ]]; then bayer=""; fi
 
 # WEBP pixel format and lossy quality
 if [[ "$filetype" == "webp" && -n "$webp_lossy" ]]; then
-	webp_lossy="-lossless 0 -quality $webp_lossy -pix_fmt yuva420p"
-elif [[ "$filetype" == "webp" && -z "$webp_lossy" ]]; then 
+	webp_lossy="-lossless 0 -pix_fmt yuva420p -quality $webp_lossy_q"
+elif [[ "$filetype" == "webp" && -z "$webp_lossy" ]]; then
 	webp_lossy="-lossless 1"
 fi
 
 # Executing the encoding command
 echo ${GREEN}"Encoding animation..."${OFF}
-ffmpeg -v "${loglevel}" ${trim:-} -i "${input}" -thread_queue_size 512 -i "${palette}" -lavfi "${filters} [x]; [x][1:v] ${decode}${errordiff}${ditherenc}${bayer}" -f "${filetype}" ${webp_lossy:-} -loop 0 -plays 0 -y "${output}"
+ffmpeg -v "${loglevel}" ${trim:-} -i "${input}" -thread_queue_size 512 -i "${palette}" \
+-lavfi "${filters} [x]; [x][1:v] ${decode}${errordiff}${ditherenc}${bayer}" \
+-f "${filetype}" ${webp_lossy:-} -loop 0 -plays 0 -y "${output}"
 
 # Checking if output file was created
 if [[ ! -f "$output" ]]; then
@@ -259,13 +277,13 @@ fi
 
 echo ${GREEN}"Done."${OFF}
 
-} 
+}
 ### End Main ###
 
 ### Function to print the help message ###
 print_help() {
 cat << EOF
-${GREEN}Video to GIF/APNG/WEBP converter v6.0${OFF}
+${GREEN}Video to GIF/APNG/WEBP converter v6.1${OFF}
 ${BLUE}By MDHEXT, Nabi KaramAliZadeh, Pathduck${OFF}
 
 ${GREEN}Usage:${OFF}
@@ -280,10 +298,13 @@ ${GREEN}Arguments:${OFF}
   -c  Maximum colors usable per palette, range 3-256 (default 256)
   -s  Start time of the animation (HH:MM:SS.MS)
   -e  End time of the animation (HH:MM:SS.MS)
+  -x  Crop the input video (out_w:out_h:x:y)
   -d  Dithering algorithm to be used (default 0)
   -b  Bayer Scale setting, range 0-5 (default 2)
   -m  Palettegen mode: 1 (diff, default), 2 (single), 3 (full)
   -k  Enables paletteuse error diffusion
+  -y  Preview animation using 'ffplay' (part of ffmpeg)
+      (Useful for testing cropping, will not give exact start/end times)
   -p  Opens the resulting animation in the default image viewer
   -v  Set FFmpeg log level (default: error)
 
