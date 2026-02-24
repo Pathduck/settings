1db9550ca119faa9f3d6b517dc40e26d4e283f90
Stian <stianlund@gmail.com>
2026-02-24 19:47:00 +0100

    Update files

--- Vivaldi/backup-vivaldi.cmd
+++ Vivaldi/backup-vivaldi.cmd
@@ -1,20 +1,24 @@
 @echo off
-set vivaldi_dir="%localappdata%\Vivaldi\User Data\Default"
-set backup_dir="%documents%\Settings\Vivaldi\backup"
+set "vivaldi_dir=%localappdata%\Vivaldi\User Data\Default"
+set "backup_dir=%documents%\Settings\Vivaldi\backup"
+
+if not exist "%backup_dir%" (
+	mkdir "%backup_dir%"
+)
 
 pushd .
-cd /d %vivaldi_dir%
+cd /d "%vivaldi_dir%"
 
-zip -u %backup_dir%\Profile.zip Bookmarks Preferences "Secure Preferences" Notes "Web Data"
+zip -u "%backup_dir%\Profile.zip" Bookmarks Preferences "Secure Preferences" Notes "Web Data"
 
-xcopy /qy contextmenu.json %backup_dir%
-xcopy /qy mainmenu.json %backup_dir%
+xcopy /qy contextmenu.json "%backup_dir%"
+xcopy /qy mainmenu.json "%backup_dir%"
 
-jq '.vivaldi' Preferences > %backup_dir%\vivaldi-settings.json
+jq '.vivaldi' Preferences > "%backup_dir%\vivaldi-settings.json"
 
-xcopy /qy "Web Data" %backup_dir%
-sqlite3 "%backup_dir%\Web Data" -line "select short_name, keyword, url, suggest_url, image_url, search_url_post_params, suggest_url_post_params, image_url_post_params from keywords;" > %backup_dir%\keywords.txt
-sqlite3 "%backup_dir%\Web Data" -markdown "select short_name, keyword, url, suggest_url, image_url, search_url_post_params, suggest_url_post_params, image_url_post_params from keywords;" > %backup_dir%\keywords.md
-sqlite3 "%backup_dir%\Web Data" ".dump keywords" > %backup_dir%\keywords.sql
-del %backup_dir%\"Web Data"
+xcopy /qy "Web Data" "%backup_dir%"
+sqlite3 "%backup_dir%\Web Data" -line "select short_name, keyword, url, suggest_url, image_url, search_url_post_params, suggest_url_post_params, image_url_post_params from keywords;" > "%backup_dir%\keywords.txt"
+sqlite3 "%backup_dir%\Web Data" -markdown "select short_name, keyword, url, suggest_url, image_url, search_url_post_params, suggest_url_post_params, image_url_post_params from keywords;" > "%backup_dir%\keywords.md"
+sqlite3 "%backup_dir%\Web Data" ".dump keywords" > "%backup_dir%\keywords.sql"
+del "%backup_dir%\Web Data"
 popd
